59ef197d57fc8515a4e9b04effb029dd4060c3a6
Linus Romland <hello@linusromland.com>
2024-08-09 10:52:38 +0200

    remove rust

--- plugin_interface/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub trait WallpaperPlugin {
-    fn get_image(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
-}
-
-pub type PluginCreateFn = fn() -> *mut dyn WallpaperPlugin;
--- plugins/plugin_sample/winDev.sh
+++
@@ -1,5 +0,0 @@
-echo "Building plugin_sample for Windows"
-cargo build
-echo "Plugin built. Copying to wallpaper_changer"
-cp target/debug/plugin_sample.dll ../../wallpaper_changer/src/plugins/plugin_sample.dll
-echo "Plugin copied. Done"
