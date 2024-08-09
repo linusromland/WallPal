2afb4b4bca274634bc8718abf91ed521b04a163b
Linus Romland <hello@linusromland.com>
2024-08-09 15:06:43 +0200

    created plugin for traffic camera

---
+++ Plugins/TrafficCameraProxy/README.md
@@ -0,0 +1,5 @@
+# TrafficCameraProxy
+
+The `TrafficCameraProxy` is a plugin that gets the latest image from a traffic camera. It is using [`Markussim's`](https://github.com/Markussim) (`traffic-cameras-server`)(https://github.com/Markussim/traffic-cameras-server) to get the latest image from a traffic camera.
+
+You can specify the camera you want to get the image from by providing the camera's ID. It is also possible to change the server URL in the plugin config.
---
+++ Plugins/TrafficCameraProxy/TrafficCameraProxy.cs
@@ -0,0 +1,83 @@
+﻿using Interfaces;
+using Newtonsoft.Json.Linq;
+
+
+namespace TrafficCameraProxy
+{
+    public struct ConfigStruct
+    {
+        public string serverURL;
+        public string cameraId;
+    }
+
+    public class TrafficCameraProxy(IApplicationServices appServices) : IPlugin
+    {
+        private readonly IApplicationServices _appServices = appServices;
+        public string Name { get; } = "TrafficCameraProxy";
+
+
+        public bool IsReady()
+        {
+            string? serverURL = GetURL();
+            if (serverURL == null)
+            {
+                return false;
+            }
+
+            using HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(serverURL).Result;
+
+                return response.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public JObject GetDefaultConfig()
+        {
+            ConfigStruct config = new ConfigStruct
+            {
+                serverURL = "https://trafficcamera.markus.romland.dev/",
+                cameraId = "SE_STA_CAMERA_Orion_4500003"
+            };
+
+            return JObject.FromObject(config);
+        }
+
+        private string? GetURL()
+        {
+            JObject config = _appServices.GetConfig(GetDefaultConfig());
+            string? serverURL = config?["serverURL"]?.ToString();
+            string? cameraId = config?["cameraId"]?.ToString();
+
+            if (serverURL == null || cameraId == null)
+            {
+                return null;
+            }
+
+            if (serverURL.EndsWith("/"))
+            {
+                serverURL = serverURL.Substring(0, serverURL.Length - 1);
+            }
+
+            return $"{serverURL}/{cameraId}/latest";
+        }
+
+
+
+        public Stream GetWallpaperStream()
+        {
+            string? url = GetURL() ?? throw new Exception("Missing serverURL or cameraId in config");
+
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStream();
+        }
+
+    }
+}
